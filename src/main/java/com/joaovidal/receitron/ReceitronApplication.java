df185cb9a060ef6da2e19dafeb06d02532f6cd3d
João Vidal <dev.joaovidal@gmail.com>
2025-09-06 14:19:57 -0300

    create project

---
+++ src/main/java/com/joaovidal/receitron/ReceitronApplication.java
@@ -0,0 +1,13 @@
+package com.joaovidal.receitron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaovidal/receitron/ReceitronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovidal.receitron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
