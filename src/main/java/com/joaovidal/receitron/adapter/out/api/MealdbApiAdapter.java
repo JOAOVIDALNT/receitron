884b937a3a2870ad564503ccce6e5ade0a6b3843
João Vidal <dev.joaovidal@gmail.com>
2025-09-09 15:44:14 -0300

    'feat: handle possible exceptions when external api doesn't return data'

--- src/main/java/com/joaovidal/receitron/adapter/out/api/MealdbApiAdapter.java
+++ src/main/java/com/joaovidal/receitron/adapter/out/api/MealdbApiAdapter.java
@@ -7,8 +7,10 @@ import com.joaovidal.receitron.domain.port.out.MealdbApiPort;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
+import org.springframework.web.client.*;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.List;
 
@@ -27,16 +29,25 @@ public class MealdbApiAdapter implements MealdbApiPort {
                 .baseUrl("https://www.themealdb.com/api/json/v1/1/")
                 .build();
 
-        CultureResponse result = restClient.get()
-                .uri("list.php?a=list")
-                .retrieve()
-                .body(CultureResponse.class);
+        try {
+            CultureResponse result = restClient.get()
+                    .uri("list.php?a=list")
+                    .retrieve()
+                    .body(CultureResponse.class);
 
-        if (result == null || result.meals() == null) {
-            throw new ApiException("Failed to fetch categories", HttpStatus.BAD_GATEWAY);
+            if (result == null || result.meals() == null) {
+                throw new ApiException("Failed to fetch categories", HttpStatus.BAD_GATEWAY);
+            }
+
+            return result.meals().stream().map(CultureObject::culture).toList();
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            throw new ApiException("Error fetching data from mealdb api", HttpStatus.BAD_GATEWAY);
+        } catch (RestClientException e) {
+            throw new ApiException("Error communicating with mealdb api", HttpStatus.BAD_GATEWAY);
+        } catch (Exception e) {
+            throw new ApiException("Unknow error on mealdb api request", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        return result.meals().stream().map(CultureObject::culture).toList();
     }
 
     @Override
@@ -45,81 +56,123 @@ public class MealdbApiAdapter implements MealdbApiPort {
                 .baseUrl("https://www.themealdb.com/api/json/v1/1/")
                 .build();
 
-        CategoryResponse result = restClient.get()
-                .uri("list.php?c=list")
-                .retrieve()
-                .body(CategoryResponse.class);
+        try {
+            CategoryResponse result = restClient.get()
+                    .uri("list.php?c=list")
+                    .retrieve()
+                    .body(CategoryResponse.class);
 
-        if (result == null || result.meals() == null) {
-            throw new ApiException("Failed to fetch categories", HttpStatus.BAD_GATEWAY);
+            if (result == null || result.meals() == null) {
+                throw new ApiException("Failed to fetch categories", HttpStatus.BAD_GATEWAY);
+            }
+
+            return result.meals().stream().map(CategoryObject::category).toList();
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            throw new ApiException("Error fetching data from mealdb api", HttpStatus.BAD_GATEWAY);
+        } catch (RestClientException e) {
+            throw new ApiException("Error communicating with mealdb api", HttpStatus.BAD_GATEWAY);
+        } catch (Exception e) {
+            throw new ApiException("Unknow error on mealdb api request", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        return result.meals().stream().map(CategoryObject::category).toList();
     }
 
     @Override
     public Recipe getRandomRecipe() {
-        var result =  webClient.get()
-                .uri("random.php")
-                .retrieve()
-                .bodyToMono(RecipeResponse.class)
-                .block();
+        try {
+            var result =  webClient.get()
+                    .uri("random.php")
+                    .retrieve()
+                    .bodyToMono(RecipeResponse.class)
+                    .block();
 
-        if (result == null || result.recipe() == null) {
-            throw new ApiException("Failed to fetch recipes", HttpStatus.BAD_GATEWAY);
+            if (result == null || result.recipe() == null) {
+                throw new ApiException("Failed to fetch recipes", HttpStatus.BAD_GATEWAY);
+            }
+
+            return result.recipe().stream().findFirst()
+                    .map(x -> new Recipe(x.id(), x.title(), x.category(), x.culture(), x.instructions()))
+                    .orElseThrow(() -> new ApiException("No recipe found", HttpStatus.NOT_FOUND));
+
+        } catch (WebClientResponseException e) {
+            throw new ApiException("Error fetching data from mealdb api", HttpStatus.BAD_GATEWAY);
+        } catch (WebClientRequestException e) {
+            throw new ApiException("Error communicating with mealdb api", HttpStatus.BAD_GATEWAY);
+        } catch (Exception e) {
+            throw new ApiException("Unknow error on mealdb api request", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        return result.recipe().stream().findFirst()
-                .map(x -> new Recipe(x.id(), x.title(), x.category(), x.culture(), x.instructions()))
-                .orElseThrow(() -> new ApiException("No recipe found", HttpStatus.NOT_FOUND));
     }
 
     @Override
-//    @Cacheable(cacheNames = "recipesByCulture", key = "#culture")
     public List<Recipe> getRecipesByCulture(String culture) {
-        var result =  webClient.get()
-                .uri("filter.php?a="+culture)
-                .retrieve()
-                .bodyToMono(SimpleRecipeResponse.class)
-                .block();
+        try {
+            var result = webClient.get()
+                    .uri("filter.php?a=" + culture)
+                    .retrieve()
+                    .bodyToMono(SimpleRecipeResponse.class)
+                    .block();
 
-        if (result == null || result.recipe() == null) {
-            throw new ApiException("Failed to fetch recipes", HttpStatus.BAD_GATEWAY);
+            if (result == null || result.recipe() == null) {
+                throw new ApiException("Failed to fetch recipes", HttpStatus.BAD_GATEWAY);
+            }
+
+            return result.recipe().stream().map(x -> new Recipe(x.id(), x.title())).toList();
+
+        } catch (WebClientResponseException e) {
+            throw new ApiException("Error fetching data from mealdb api", HttpStatus.BAD_GATEWAY);
+        } catch (WebClientRequestException e) {
+            throw new ApiException("Error communicating with mealdb api", HttpStatus.BAD_GATEWAY);
+        } catch (Exception e) {
+            throw new ApiException("Unknow error on mealdb api request", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        return result.recipe().stream().map(x -> new Recipe(x.id(), x.title())).toList();
     }
 
     @Override
-//    @Cacheable(cacheNames = "recipesByCategory", key = "#category")
     public List<Recipe> getRecipesByCategory(String category) {
-        var result =  webClient.get()
-                .uri("filter.php?c="+category)
-                .retrieve()
-                .bodyToMono(SimpleRecipeResponse.class)
-                .block();
+        try {
+            var result = webClient.get()
+                    .uri("filter.php?c=" + category)
+                    .retrieve()
+                    .bodyToMono(SimpleRecipeResponse.class)
+                    .block();
 
-        if (result == null || result.recipe() == null) {
-            throw new ApiException("Failed to fetch recipe", HttpStatus.BAD_GATEWAY);
+            if (result == null || result.recipe() == null) {
+                throw new ApiException("Failed to fetch recipe", HttpStatus.BAD_GATEWAY);
+            }
+
+            return result.recipe().stream().map(x -> new Recipe(x.id(), x.title())).toList();
+
+        } catch (WebClientResponseException e) {
+            throw new ApiException("Error fetching data from mealdb api", HttpStatus.BAD_GATEWAY);
+        } catch (WebClientRequestException e) {
+            throw new ApiException("Error communicating with mealdb api", HttpStatus.BAD_GATEWAY);
+        } catch (Exception e) {
+            throw new ApiException("Unknow error on mealdb api request", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        return result.recipe().stream().map(x -> new Recipe(x.id(), x.title())).toList();
     }
 
     @Override
     public Recipe getRecipeById(int id) {
-        var result =  webClient.get()
-                .uri("lookup.php?i="+id)
-                .retrieve()
-                .bodyToMono(RecipeResponse.class)
-                .block();
+        try {
+            var result = webClient.get()
+                    .uri("lookup.php?i=" + id)
+                    .retrieve()
+                    .bodyToMono(RecipeResponse.class)
+                    .block();
 
-        if (result == null || result.recipe() == null) {
-            throw new ApiException("Failed to fetch recipe", HttpStatus.BAD_GATEWAY);
+            if (result == null || result.recipe() == null) {
+                throw new ApiException("Failed to fetch recipe", HttpStatus.BAD_GATEWAY);
+            }
+
+            return result.recipe().stream().findFirst()
+                    .map(x -> new Recipe(x.id(), x.title(), x.category(), x.culture(), x.instructions()))
+                    .orElseThrow(() -> new ApiException("No recipe found", HttpStatus.NOT_FOUND));
+
+        } catch (WebClientResponseException e) {
+            throw new ApiException("Error fetching data from mealdb api", HttpStatus.BAD_GATEWAY);
+        } catch (WebClientRequestException e) {
+            throw new ApiException("Error communicating with mealdb api", HttpStatus.BAD_GATEWAY);
+        } catch (Exception e) {
+            throw new ApiException("Unknow error on mealdb api request", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        return result.recipe().stream().findFirst()
-                .map(x -> new Recipe(x.id(), x.title(), x.category(), x.culture(), x.instructions()))
-                .orElseThrow(() -> new ApiException("No recipe found", HttpStatus.NOT_FOUND));
     }
 }
